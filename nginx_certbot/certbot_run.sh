7560dac96a5ee5026fd9dcadc89a0733bae4d5e7
dino2895 <dino2895un@gmail.com>
2025-06-05 13:06:48 +0000

    remove docker nginx

--- nginx_certbot/certbot_run.sh
+++ nginx_certbot/certbot_run.sh
@@ -5,7 +5,7 @@ set -e
 sleep 5
 
 # 嘗試獲取憑證
-certbot --nginx --agree-tos --no-interactive -d iot.rm-rf.uk -m dino2895un@gmail.com
+certbot --nginx --agree-tos --noninteractive -d iot.rm-rf.uk -m dino2895un@gmail.com
 
 # 如果憑證不存在，則退出並顯示錯誤
 if [ ! -f /etc/letsencrypt/live/iot.rm-rf.uk/fullchain.pem ]; then
